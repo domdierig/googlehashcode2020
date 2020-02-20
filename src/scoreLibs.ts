73d506d4271eaea6a0b0a6357324492264ed3539
Dominic <dom.pilipu@outlook.de>
2020-02-20 21:45:36 +0100

    modified score calc

--- src/scoreLibs.ts
+++ src/scoreLibs.ts
@@ -6,5 +6,6 @@ export const scoreLibs = (context: Context) => {
         lib.score += lib.shippingRate * -1;
         let bookDelta = context.totalBooks - lib.totalBooks;
         lib.score += bookDelta;
+        lib.score += lib.shippingRate * (context.daysForScanning - lib.signupProcess) * -1
     }
 }
