716fadb4fd89e1987c04940d54f0c25e96f91d73
Marc Vornetran <marc.vornetran@gmail.com>
2020-02-20 20:16:53 +0100

    Fix assumption about books and scores

--- src/parseInput.ts
+++ src/parseInput.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { Context } from './Context';
 import { Library } from './Library';
+import { Book } from './Book';
 
 export function parseInput(fileName: string): Context {
     const filePath: string = path.join('input', fileName);
@@ -10,26 +11,27 @@ export function parseInput(fileName: string): Context {
         .split('\n')
         .filter((line: string) => line.length > 0);
     const [totalBooks, totalLibraries, daysForScanning] = splitAndParseInt(firstLine);
-    const books: number[] = splitAndParseInt(secondLine);
+    const bookScores: number[] = splitAndParseInt(secondLine);
     const libraries: Library[] = [];
     let nextLibraryId: number = 0;
 
     for (let i = 0; i < lines.length - 1; i += 2) {
         const libraryContext: string = lines[i];
         const libraryBooks: string = lines[i + 1];
-        const library: Library = parseLibrary(nextLibraryId++, libraryContext, libraryBooks);
+        const library: Library = parseLibrary(nextLibraryId++, libraryContext, libraryBooks, bookScores);
 
         libraries.push(library);
     }
 
     return new Context(
-        totalBooks, totalLibraries, daysForScanning, books, libraries
+        totalBooks, totalLibraries, daysForScanning, bookScores, libraries
     );
 }
 
-function parseLibrary(libraryId: number, libraryContext: string, libraryBooks: string): Library {
+function parseLibrary(libraryId: number, libraryContext: string, libraryBooks: string, bookScores: number[]): Library {
     const [totalBooks, signupProcess, shippingSpeed] = splitAndParseInt(libraryContext);
-    const books: number[] = splitAndParseInt(libraryBooks);
+    const bookIds: number[] = splitAndParseInt(libraryBooks);
+    const books: Book[] = bookIds.map((bookId: number) => new Book(bookId, bookScores[bookId]));
 
     return new Library(
         libraryId, totalBooks, signupProcess, shippingSpeed, books
